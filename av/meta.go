7b6e03636bd5dfd4071c6511df3ce19a8e1d2ea0
notch <hjcao_wei@163.com>
2020-12-10 13:36:31 +0800

    add avmeta

---
+++ av/meta.go
@@ -0,0 +1,27 @@
+// Copyright (c) 2019,CAOHONGJU All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package av
+
+// VideoMeta 视频元数据
+type VideoMeta struct {
+	Codec     string  `json:"codec"`
+	DataRate  int     `json:"datarate,omitempty"`
+	Width     int     `json:"width,omitempty"`
+	Height    int     `json:"height,omitempty"`
+	FrameRate float64 `json:"framerate,omitempty"`
+	Sps       []byte  `json:"-"`
+	Pps       []byte  `json:"-"`
+	Vps       []byte  `json:"-"`
+}
+
+// AudioMeta 音频元数据
+type AudioMeta struct {
+	Codec      string `json:"codec"`
+	DataRate   int    `json:"datarate,omitempty"`
+	SampleRate int    `json:"samplerate,omitempty"`
+	SampleSize int    `json:"samplesize,omitempty"`
+	Stereo     bool   `json:"stereo,omitempty"`
+	Sps        []byte `json:"-"` // sps
+}
