87fd989cfa08d03627ddb05befc729b3d7c6fb9e
notch <hjcao_wei@163.com>
2020-12-23 14:40:40 +0800

    add flvmuxer

---
+++ media/flvmuxer.go
@@ -0,0 +1,346 @@
+// Copyright (c) 2019,CAOHONGJU All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package media
+
+import (
+	"runtime/debug"
+	"time"
+
+	"github.com/cnotch/ipchub/av"
+	"github.com/cnotch/ipchub/av/h264"
+	"github.com/cnotch/ipchub/media/cache"
+	"github.com/cnotch/ipchub/protos/amf"
+	"github.com/cnotch/ipchub/protos/flv"
+	"github.com/cnotch/ipchub/protos/rtp"
+	"github.com/cnotch/xlog"
+)
+
+type flvMuxer struct {
+	videoMeta         av.VideoMeta
+	audioMeta         av.AudioMeta
+	hasAudio          bool
+	audioDataTemplate *flv.AudioData
+	recvQueue         *cache.PackQueue
+	h264Extractor     rtp.FrameExtractor
+	mpesExtractor     rtp.FrameExtractor
+	tagWriter         flv.TagWriter
+	closed            bool
+	spsMuxed          bool
+	baseNtp           int64
+	baseTs            int64
+	logger            *xlog.Logger // 日志对象
+}
+
+func newRtp2Flv(videoMeta av.VideoMeta, audioMeta av.AudioMeta, tagWriter flv.TagWriter, logger *xlog.Logger) *flvMuxer {
+	muxer := &flvMuxer{
+		recvQueue: cache.NewPackQueue(),
+		videoMeta: videoMeta,
+		audioMeta: audioMeta,
+		hasAudio:  audioMeta.Codec == "AAC",
+		tagWriter: tagWriter,
+		closed:    false,
+		logger:    logger,
+	}
+
+	muxer.h264Extractor = rtp.NewH264FrameExtractor(muxer)
+	if muxer.hasAudio {
+		muxer.mpesExtractor = rtp.NewMPESFrameExtractor(muxer, audioMeta.SampleRate)
+		muxer.prepareTemplate()
+	}
+
+	go muxer.consume()
+	return muxer
+}
+
+func (muxer *flvMuxer) prepareTemplate() {
+	audioData := &flv.AudioData{
+		SoundFormat:   flv.SoundFormatAAC,
+		AACPacketType: flv.AACPacketTypeRawData,
+		Body:          nil,
+	}
+
+	switch muxer.audioMeta.SampleRate {
+	case 5512:
+		audioData.SoundRate = flv.SoundRate5512
+	case 11025:
+		audioData.SoundRate = flv.SoundRate11025
+	case 22050:
+		audioData.SoundRate = flv.SoundRate22050
+	case 44100:
+		audioData.SoundRate = flv.SoundRate44100
+	default:
+		audioData.SoundRate = flv.SoundRate44100
+	}
+
+	if muxer.audioMeta.SampleSize == 8 {
+		audioData.SoundSize = flv.SoundeSize8bit
+	} else {
+		audioData.SoundSize = flv.SoundeSize16bit
+	}
+
+	if muxer.audioMeta.Channels > 1 {
+		audioData.SoundType = flv.SoundTypeStereo
+	} else {
+		audioData.SoundType = flv.SoundTypeMono
+	}
+
+	muxer.audioDataTemplate = audioData
+}
+
+func (muxer *flvMuxer) WriteFrame(frame *rtp.Frame) error {
+	if frame.FrameType == rtp.FrameVideo {
+		return muxer.muxVideoTag(frame)
+	} else {
+		return muxer.muxAudioTag(frame)
+	}
+	return nil
+}
+
+func (muxer *flvMuxer) muxVideoTag(frame *rtp.Frame) error {
+	if frame.Payload[0]&0x1F == h264.NalSps {
+		if len(muxer.videoMeta.Sps) == 0 {
+			muxer.videoMeta.Sps = frame.Payload
+		}
+		return muxer.muxSequenceHeaderTag()
+	}
+
+	if frame.Payload[0]&0x1F == h264.NalPps {
+		if len(muxer.videoMeta.Pps) == 0 {
+			muxer.videoMeta.Pps = frame.Payload
+		}
+		return muxer.muxSequenceHeaderTag()
+	}
+
+	if muxer.baseNtp == 0 {
+		muxer.baseNtp = frame.NTPTime
+		muxer.baseTs = time.Now().UnixNano() / int64(time.Millisecond)
+	}
+
+	dts := time.Now().UnixNano()/int64(time.Millisecond) - muxer.baseTs
+	videoData := &flv.VideoData{
+		FrameType:       flv.FrameTypeInterFrame,
+		CodecID:         flv.CodecIDAVC,
+		AVCPacketType:   flv.AVCPacketTypeNALU,
+		CompositionTime: uint32(frame.NTPTime - muxer.baseNtp),
+		Body:            frame.Payload,
+	}
+
+	if frame.Payload[0]&0x1F == h264.NalIdrSlice {
+		videoData.FrameType = flv.FrameTypeKeyFrame
+	}
+	data, _ := videoData.Marshal()
+
+	tag := &flv.Tag{
+		TagType:   flv.TagTypeVideo,
+		DataSize:  uint32(len(data)),
+		Timestamp: uint32(dts),
+		StreamID:  0,
+		Data:      data,
+	}
+
+	return muxer.tagWriter.WriteTag(tag)
+}
+
+func (muxer *flvMuxer) muxAudioTag(frame *rtp.Frame) error {
+	if muxer.baseNtp == 0 {
+		muxer.baseNtp = frame.NTPTime
+		muxer.baseTs = time.Now().UnixNano() / int64(time.Millisecond)
+	}
+
+	audioData := *muxer.audioDataTemplate
+	audioData.Body = frame.Payload
+	data, _ := audioData.Marshal()
+
+	tag := &flv.Tag{
+		TagType:   flv.TagTypeAudio,
+		DataSize:  uint32(len(data)),
+		Timestamp: uint32(frame.NTPTime - muxer.baseNtp),
+		StreamID:  0,
+		Data:      data,
+	}
+	return muxer.tagWriter.WriteTag(tag)
+}
+
+func (muxer *flvMuxer) muxMetadataTag() error {
+	properties := make(amf.EcmaArray, 0, 12)
+
+	if muxer.hasAudio {
+		properties = append(properties,
+			amf.ObjectProperty{flv.MetaDataAudioCodecID, flv.SoundFormatAAC})
+		properties = append(properties,
+			amf.ObjectProperty{flv.MetaDataAudioDateRate, muxer.audioMeta.DataRate})
+		properties = append(properties,
+			amf.ObjectProperty{flv.MetaDataAudioSampleRate, muxer.audioMeta.SampleRate})
+		properties = append(properties,
+			amf.ObjectProperty{flv.MetaDataAudioSampleSize, muxer.audioMeta.SampleSize})
+		properties = append(properties,
+			amf.ObjectProperty{flv.MetaDataStereo, muxer.audioMeta.Channels > 1})
+	}
+
+	properties = append(properties,
+		amf.ObjectProperty{flv.MetaDataFrameRate, muxer.videoMeta.FrameRate})
+	properties = append(properties,
+		amf.ObjectProperty{flv.MetaDataHeight, muxer.videoMeta.Height})
+	properties = append(properties,
+		amf.ObjectProperty{flv.MetaDataVideoCodecID, flv.CodecIDAVC})
+	properties = append(properties,
+		amf.ObjectProperty{flv.MetaDataHeight, muxer.videoMeta.Height})
+	properties = append(properties,
+		amf.ObjectProperty{flv.MetaDataVideoDataRate, muxer.videoMeta.DataRate})
+	properties = append(properties,
+		amf.ObjectProperty{flv.MetaDataWidth, muxer.videoMeta.Width})
+
+	scriptData := flv.ScriptData{
+		Name:  flv.ScriptOnMetaData,
+		Value: properties,
+	}
+	data, _ := scriptData.Marshal()
+
+	tag := &flv.Tag{
+		TagType:   flv.TagTypeAmf0Data,
+		DataSize:  uint32(len(data)),
+		Timestamp: 0,
+		StreamID:  0,
+		Data:      data,
+	}
+
+	return muxer.tagWriter.WriteTag(tag)
+}
+
+func (muxer *flvMuxer) muxSequenceHeaderTag() error {
+	if muxer.spsMuxed {
+		return nil
+	}
+
+	if len(muxer.videoMeta.Sps) == 0 || len(muxer.videoMeta.Pps) == 0 {
+		// not enough
+		return nil
+	}
+
+	muxer.spsMuxed = true
+
+	record := &flv.AVCDecoderConfigurationRecord{
+		ConfigurationVersion: 1,
+		AVCProfileIndication: muxer.videoMeta.Sps[1],
+		ProfileCompatibility: muxer.videoMeta.Sps[2],
+		AVCLevelIndication:   muxer.videoMeta.Sps[3],
+		SPS:                  muxer.videoMeta.Sps,
+		PPS:                  muxer.videoMeta.Pps,
+	}
+	body, _ := record.Marshal()
+
+	videoData := &flv.VideoData{
+		FrameType:       flv.FrameTypeKeyFrame,
+		CodecID:         flv.CodecIDAVC,
+		AVCPacketType:   flv.AACPacketTypeSequenceHeader,
+		CompositionTime: 0,
+		Body:            body,
+	}
+	data, _ := videoData.Marshal()
+
+	tag := &flv.Tag{
+		TagType:   flv.TagTypeVideo,
+		DataSize:  uint32(len(data)),
+		Timestamp: 0,
+		StreamID:  0,
+		Data:      data,
+	}
+
+	if err := muxer.tagWriter.WriteTag(tag); err != nil {
+		return err
+	}
+
+	return muxer.muxAudioSequenceHeaderTag()
+}
+
+func (muxer *flvMuxer) muxAudioSequenceHeaderTag() error {
+	if !muxer.hasAudio {
+		return nil
+	}
+
+	audioData := *muxer.audioDataTemplate
+	audioData.AACPacketType = flv.AACPacketTypeSequenceHeader
+	audioData.Body = muxer.audioMeta.Sps
+	data, _ := audioData.Marshal()
+
+	tag := &flv.Tag{
+		TagType:   flv.TagTypeAudio,
+		DataSize:  uint32(len(data)),
+		Timestamp: 0,
+		StreamID:  0,
+		Data:      data,
+	}
+	return muxer.tagWriter.WriteTag(tag)
+}
+
+func (muxer *flvMuxer) consume() {
+	defer func() {
+		defer func() { // 避免 handler 再 panic
+			recover()
+		}()
+
+		if r := recover(); r != nil {
+			muxer.logger.Errorf("flvmuxer routine panic；r = %v \n %s", r, debug.Stack())
+		}
+
+		// 尽早通知GC，回收内存
+		muxer.recvQueue.Clear()
+	}()
+
+	muxer.muxMetadataTag()
+	muxer.muxSequenceHeaderTag()
+
+	for !muxer.closed {
+		pack := muxer.recvQueue.Dequeue()
+		if pack == nil {
+			if !muxer.closed {
+				muxer.logger.Warn("flvmuxer:receive nil packet")
+			}
+			continue
+		}
+
+		packet := pack.(*rtp.Packet)
+		if muxer.hasAudio {
+			switch packet.Channel {
+			case rtp.ChannelAudio:
+				if err := muxer.mpesExtractor.Extract(packet); err != nil {
+					muxer.logger.Errorf("extract aac error :%s", err.Error())
+				}
+			case rtp.ChannelVideo:
+				if err := muxer.h264Extractor.Extract(packet); err != nil {
+					muxer.logger.Errorf("extract h264 error :%s", err.Error())
+				}
+			case rtp.ChannelVideoControl:
+				muxer.h264Extractor.Control(packet)
+			case rtp.ChannelAudioControl:
+				muxer.mpesExtractor.Control(packet)
+			}
+		} else {
+			switch packet.Channel {
+			case rtp.ChannelVideo:
+				if err := muxer.h264Extractor.Extract(packet); err != nil {
+					muxer.logger.Errorf("extract h264 error :%s", err.Error())
+				}
+			case rtp.ChannelVideoControl:
+				muxer.h264Extractor.Control(packet)
+			}
+		}
+	}
+}
+
+func (muxer *flvMuxer) Close() error {
+	if muxer.closed {
+		return nil
+	}
+
+	muxer.closed = true
+	muxer.recvQueue.Signal()
+	return nil
+}
+
+func (muxer *flvMuxer) WritePacket(packet *rtp.Packet) error {
+	muxer.recvQueue.Enqueue(packet)
+	return nil
+}
